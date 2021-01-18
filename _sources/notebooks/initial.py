73ae4ec9a0e12574b0648d57957098fbc1fe66d4
Gautam Sisodia <gautam.sisodia@gmail.com>
2021-01-18 12:36:58 -0500

    Update documentation

--- _sources/notebooks/initial.py
+++ _sources/notebooks/initial.py
@@ -2,3 +2,29 @@
 """
 # Initial exploration of the data
 """
+
+# %% tags=['hide-cell']
+from IPython import get_ipython
+
+if get_ipython() is not None:
+    get_ipython().run_line_magic("load_ext", "autoreload")
+    get_ipython().run_line_magic("autoreload", "2")
+
+# %%
+import pandas as pd
+from babytimes import data as d
+from pygsutils import general as g
+import altair as alt
+
+# %%
+st = d.Sleeptimes({"loc": "./../../data"})
+
+# %%
+st.df_viz.head()
+
+# %%
+bars = alt.Chart(st.df_viz).mark_bar().encode(
+    y="yearmonthdate(begin)",
+    x="hoursminutesseconds(begin)",
+    x2="hoursminutesseconds(end)",
+)
